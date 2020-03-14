7e67d77ed4381b0671a87f9678b4bc43ac648fcc
DAVID ALONSO <d.alonso.sanchez@accenture.com>
2020-03-14 12:54:59 +0100

    Inicio proyecto prestamos back

---
+++ src/test/java/es/vir2al/prestamos/PrestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package es.vir2al.prestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
